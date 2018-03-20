3760b6a7418d670b4ccb0e6a5b865477d510a856
leemove <923522454@qq.com>
2018-03-20 11:37:09 +0800

    demo

--- 2018-03-25/koajs/app.js
+++ 2018-03-25/koajs/app.js
@@ -1,10 +1,25 @@
 const koa = require('koa')
 const app = new koa()
 
+app.use(async (ctx, next) => {
+  const startTime = new Date()
+  await next()
+  console.log(2)
+  const timeSpan = new Date() - startTime
+  ctx.set('X-Response-Time', timeSpan + 'ms')
+})
+
+app.use(async (ctx, next) => {
+  console.log(3)
+  await next()
+  console.log(4)
+})
+
 app.use(ctx => {
+  console.log(1)
   ctx.body = "<h1>Hello Koa</h1>"
 })
 
 app.listen('8888', () => {
-  console.log('服务启动,监听8888端口')
+  console.log('服务启动,监听8888端口\n地址为:http://localhost:8888/')
 })
